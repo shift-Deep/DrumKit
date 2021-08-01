8c1e2b1aa2487c235fbdd27f1e65ebb428c53de6
shift-Deep <cs17aman97@gmail.com>
2021-08-01 20:52:24 +0530

    upload

---
+++ index.js
@@ -0,0 +1,58 @@
+console.log('hello');
+
+for (var i = 0; i < document.querySelectorAll('.drum').length ; i++){
+    document.querySelectorAll('.drum')[i].addEventListener('click',function(){
+        buttonText = this.innerHTML;
+        playsound(buttonText)
+        setAnimation(buttonText)        
+                
+    })
+}
+
+document.addEventListener('keydown' ,function(event){
+    key = event.key
+    playsound(key);
+    setAnimation(key)
+    
+
+});
+
+function playsound(action){
+    switch(action){
+        case 'w':
+            var audio = new Audio('sounds/tom-1.mp3');
+            audio.play();
+            break;
+        case 'a':
+            var audio = new Audio('sounds/tom-2.mp3');
+            audio.play();
+            break;
+        case 's':
+            var audio = new Audio('sounds/tom-3.mp3');
+            audio.play();
+            break;
+        case 'd':
+            var audio = new Audio('sounds/tom-4.mp3');
+            audio.play();
+            break;
+        case 'j':
+            var audio = new Audio('sounds/snare.mp3');
+            audio.play();
+            break;
+        case 'k':
+            var audio = new Audio('sounds/crash.mp3');
+            audio.play();
+            break;
+        case 'l':
+            var audio = new Audio('sounds/kick-bass.mp3');
+            audio.play();
+            break;
+    }
+}
+
+function setAnimation(act){
+    document.querySelector('.'+act).classList.add('pressed');
+    setTimeout(function(){
+        document.querySelector('.'+act).classList.remove('pressed');
+    },100)
+}
